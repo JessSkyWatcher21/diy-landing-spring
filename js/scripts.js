2fefd36d8042c636e1475d58e8351a71a45e09a2
EWU Web.Lab <pmanikoth@ewu.edu>
2022-04-13 12:53:05 -0700

    Remove Solutions

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,2 @@
-console.log('Hello World, this is my me!');
-
-const editables = document.querySelectorAll("[contenteditable]");
-
-// save edits
-editables.forEach(el => {
-  el.addEventListener("blur", () => {
-    localStorage.setItem("dataStorage-" + el.id, el.innerHTML);
-  })
-});
-
-// once on load
-for (var key in localStorage) {
-  if (key.includes("dataStorage-")) {
-    const id = key.replace("dataStorage-","");
-    document.querySelector("#" + id).innerHTML = localStorage.getItem(key);
-  }
-}
+// My Scripts
+console.log('Hello World, this is NOT ME!!!!!');
